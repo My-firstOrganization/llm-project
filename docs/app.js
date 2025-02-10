0b4225e19eb82bb41a1742634924e03bb142171f
soheeGit <63wlsthgml@gmail.com>
2025-02-10 14:25:09 +0900

    json

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,23 @@
 async function main(){
-    async function handleCC(){
-        document.querySelector('#box').textContent= await (await fetch("http://127.0.0.1:3000")).text();
+    async function handleCC(event){
+        event.preventDefault();
+        const url = "http://127.0.0.1:3000";
+        const formData = new FormData(document.querySelector("#ccForm"));
+        const text = formData.get("text");
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({
+                text
+            }), 
+            headers: {
+                "Content-Type":"Application/json"
+            }
+        });
+        const json = await response.json();
+        document.querySelector('#box').textContent = JSON.stringify(json);
     }
-    document.querySelector('#ccBtn').addEventListener("click", handleCC);
+    // document.querySelector('#ccBtn').addEventListener("click", handleCC);
+    document.querySelector('#ccForm').addEventListener("submit", handleCC);
 }
 
 document.addEventListener("DOMContentLoaded", main);
